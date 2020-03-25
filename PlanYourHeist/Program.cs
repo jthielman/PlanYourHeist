f142f60793b170c9c7569e7b2b9898e88928f73b
feven90 <f.mulugeta90@gmail.com>
2020-03-24 21:18:29 -0500

    phase one

---
+++ PlanYourHeist/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlanYourHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your heist!");
+
+            var teamMember1 = new TeamMember();
+
+            Console.WriteLine("Please enter the team member's first name.");
+            teamMember1.Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter the team member's skill level as an integer from 0 to 10.");
+            teamMember1.SkillLevel = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the team member's courage factor as a decimal 0.0-2.0.");
+            teamMember1.CourageFactor = decimal.Parse(Console.ReadLine());
+        }
+    }
+}
